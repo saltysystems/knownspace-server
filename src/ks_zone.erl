f809167fc17dbfb9748fa6a1d5201a6f511748b1
cmdrk <cmdrk@protonmail.com>
2022-10-10 18:35:02 -0500

    send entity snapshots instead of full entities every packet, among a bunch of other changes

--- src/ks_zone.erl
+++ src/ks_zone.erl
@@ -22,7 +22,7 @@
 %-define(SERVER(Name), {via, gproc, {n, l, {?MODULE, Name}}}).
 -define(SERVER, ?MODULE).
 % Radial boundary size
--define(DEFAULT_BOUNDARY, 2500).
+-define(DEFAULT_BOUNDARY,1024).
 % Set the default buffer depth in milliseconds
 -define(DEFAULT_BUFFER_DEPTH, 50).
 
@@ -33,7 +33,8 @@
 -define(KS_ZONE_PART, 16#2002).
 -define(KS_ZONE_INPUT, 16#2003).
 -define(KS_ZONE_XFER, 16#2004).
--define(KS_ZONE_NEW_ENTITY, 16#2010).
+-define(KS_ZONE_SNAP, 16#2005).
+-define(KS_ZONE_ENTITY, 16#2010).
 
 rpc_info() ->
     [
@@ -56,12 +57,17 @@ rpc_info() ->
         },
         #{
             opcode => ?KS_ZONE_XFER,
-            s2c_call => state_transfer,
+            s2c_call => zone_transfer,
             encoder => ks_pb
         },
         #{
-            opcode => ?KS_ZONE_NEW_ENTITY,
-            s2c_call => new_entity,
+            opcode => ?KS_ZONE_SNAP,
+            s2c_call => zone_snapshot,
+            encoder => ks_pb
+        },
+        #{
+            opcode => ?KS_ZONE_ENTITY,
+            s2c_call => entity,
             encoder => ks_pb
         }
     ].
@@ -137,7 +143,8 @@ init([]) ->
             % radial
             boundary => ?DEFAULT_BOUNDARY
         },
-    {ok, InitialZoneState}.
+    Config = #{ tick_ms => 20 },
+    {ok, InitialZoneState, Config}.
 
 handle_join(Msg, Session, State = #{gamestate_buffer := [GameState | Rest ]}) ->
     ID = ow_session:get_id(Session),
@@ -147,47 +154,65 @@ handle_join(Msg, Session, State = #{gamestate_buffer := [GameState | Rest ]}) ->
     PlayerInfo = #{handle => Handle},
     % Add the entity to the current gamestate
     {Entity, GameState1} = new_entity(Handle, ID, GameState),
-    % Let everyone know that the Player has joined
-    Reply = {'@zone', {new_entity, entity_map(Entity)}},
-    % We only ever create new gamestates on ticks, so just inject the user into
-    % the current one
-    % TODO: Consider consequences of this :) 
+    % Let everyone else know that the Player has joined
+    ow_zone:broadcast(self(), {entity, entity_map(Entity)}),
+    % Swap the head of the buffer with the new version of the GameState with
+    % the new player injected.
     GameStateBuffer = [ GameState1 | Rest ],
     State1 = State#{ gamestate_buffer := GameStateBuffer },
+    % Build a reply to the player with information about entities who joined
+    % before them.
+    ZoneXfer = #{
+        entities => get_entities(GameState)
+    },
+    Reply = {{'@', [ID]}, {zone_transfer, ZoneXfer}},
+    % Reply to the player, update the player registry and the zone state
     {Reply, {ok, Session, PlayerInfo}, State1}.
 
 handle_part(Session, State = #{gamestate_buffer := [GameState | Rest ]}) ->
     ID = ow_session:get_id(Session),
-    Player = ow_player_reg:get(ID),
-    Handle = maps:get(handle, ow_player_reg:get_info(Player)),
-    logger:notice("Player ~p:~p has left the server!", [Handle, ID]),
-    % Remove the player from the entities list
-    GameState1 = rm_entity(ID, GameState),
-    GameStateBuffer = [ GameState1 | Rest ],
-    State1 = State#{ gamestate_buffer := GameStateBuffer },
-    % Let everyone know that the Player departed
-    Msg = #{id => ID},
-    Reply = {'@zone', {part, Msg}},
-    {Reply, ok, State1}.
+    % Check if the player is still in the zone
+    case ow_player_reg:get(ID) of 
+        % For now, just check to see if the player is connected. This will need
+        % to be revisited when multizone comes online
+        {error, _} -> 
+            % Not in the zone
+            {noreply, ok, State};
+        Player -> 
+            Handle = maps:get(handle, ow_player_reg:get_info(Player)),
+            logger:notice("Player ~p:~p has left the server!", [Handle, ID]),
+            % Remove the player from the entities list
+            GameState1 = rm_entity(ID, GameState),
+            GameStateBuffer = [ GameState1 | Rest ],
+            State1 = State#{ gamestate_buffer := GameStateBuffer },
+            % Let everyone know that the Player departed
+            Msg = #{id => ID},
+            Reply = {'@zone', {part, Msg}},
+            {Reply, ok, State1}
+    end.
 
 handle_rpc(input, Msg, Session, State = #{input_buffer := InputBuffer}) ->
     % Inject the ID of the player moving into the Msg
     ID = ow_session:get_id(Session),
     Latency = ow_session:get_latency(Session),
-    Msg1 = Msg#{id => ID, lag => Latency},
+    Msg1 = Msg#{id => ID, latency => Latency},
     % Add the action to the input queue
     State1 = State#{input_buffer => [Msg1 | InputBuffer]},
     {noreply, ok, State1}.
 
-handle_tick(#{tick_rate := TRate}, State) ->
-    State1 = update_gamestate(TRate, State),
+handle_tick(TickMs, State) ->
+    Last = maps:get(last_tick, State, TickMs),
+    Now = erlang:monotonic_time(),
+    Delta = erlang:convert_time_unit(Now - Last, native, millisecond),
+    State1 = update_gamestate(Delta, State),
+    #{ gamestate_buffer := [GameState|_Rest]} = State1,
     ToXfer = #{
-        actions => [],
+        phys_updates => get_entity_phys(GameState),
         projectiles => [],
         collisions => []
     },
-    Reply = {'@zone', {state_transfer, ToXfer}},
-    {Reply, State1}.
+    Reply = {'@zone', {zone_snapshot, ToXfer}},
+    {Reply, State1#{ last_tick => Now }}.
 
 %%%====================================================================
 %%% Internal Functions
@@ -234,9 +259,18 @@ apply_inputs([], GameState0) ->
 apply_inputs([Input | Rest], GameState0) ->
     % This will have a list of maps containing the actual keysequence, ID, and
     % latency
-    #{id := ID, keys := Keys} = Input,
-    GameState1 = apply_input_per_player(ID, Keys, GameState0),
-    apply_inputs(Rest, GameState1).
+    #{id := ID, keys := Keys, latency := Latency} = Input,
+    % It's possible, apparently, to get into a state where the player is gone
+    % but the entity record hasn't been yet removed
+    case entity_by_id(ID, GameState0) of
+        false -> 
+            GameState0;
+        _ -> 
+            GameState1 = update_latency(ID, Latency, GameState0),
+            GameState2 = apply_input_per_player(ID, Keys, GameState1),
+            apply_inputs(Rest, GameState2)
+    end.
+
 
 apply_input_per_player(ID, Keys, GameState0) ->
     % We check every key that is applied during the frame
@@ -244,7 +278,6 @@ apply_input_per_player(ID, Keys, GameState0) ->
     ApplyInput =
         fun(KeyPress, GameState) ->
             Entity = entity_by_id(ID, GameState),
-            io:format("Got keypress. Entity is: ~p~n", [Entity]),
             Actions = action_table(),
             case lists:keyfind(KeyPress, 1, Actions) of
                 {KeyPress, Fun} ->
@@ -257,31 +290,16 @@ apply_input_per_player(ID, Keys, GameState0) ->
         end,
     lists:foldl(ApplyInput, GameState0, Keys).
 
+
+update_latency(ID, Latency, GameState0) ->
+    E = entity_by_id(ID, GameState0),
+    E1 = E#entity{latency=Latency},
+    update_entity(E1, GameState0).
+
+
 action_table() ->
     [
         {'IMPULSE_FWD', fun(E) ->
-            Phys = E#entity.phys,
-            #{speed_fac := Speed, max_vel := MaxV } = E#entity.stats,
-            % Get the current velocity
-            {_Pos, {Xv, Yv}, Rot} = phys_to_tuple(Phys),
-            % 1 keypress = Vector2(0,-1) * Speed
-            Vel1 = {0 * Speed, -1 * Speed},
-            % Rotate the keyed vector by the current rotation
-            {Xv2, Yv2} = ow_vector:rotate(Vel1, Rot),
-            % Subtract (negative Y is up) from the original vector
-            Vel3 = {Xv - Xv2, Yv - Yv2},
-            % Check to see if the velocity exceeds max velocity
-            Vel4 = 
-                case ow_vector:length_squared(Vel3) > math:pow(MaxV, 2) of
-                    true ->
-                        vector_scale(ow_vector:normalize(Vel3), MaxV);
-                    false ->
-                        Vel3
-                end,
-            % Update the entity
-            E#entity{phys = Phys#{vel => ow_vector:vector_map(Vel4)}}
-        end},
-        {'IMPULSE_REV', fun(E) ->
             Phys = E#entity.phys,
             #{speed_fac := Speed, max_vel := MaxV } = E#entity.stats,
             % Get the current velocity
@@ -296,7 +314,28 @@ action_table() ->
             Vel4 = 
                 case ow_vector:length_squared(Vel3) > math:pow(MaxV, 2) of
                     true ->
-                        vector_scale(ow_vector:normalize(Vel3), MaxV);
+                        ow_vector:scale(ow_vector:normalize(Vel3), MaxV);
+                    false ->
+                        Vel3
+                end,
+            E#entity{phys = Phys#{vel => ow_vector:vector_map(Vel4)}}
+        end},
+        {'IMPULSE_REV', fun(E) ->
+            Phys = E#entity.phys,
+            #{speed_fac := Speed, max_vel := MaxV } = E#entity.stats,
+            % Get the current velocity
+            {_Pos, {Xv, Yv}, Rot} = phys_to_tuple(Phys),
+            % 1 keypress = Vector2(0,-1) * Speed
+            Vel1 = {0 * Speed, -1 * Speed},
+            % Rotate the keyed vector by the current rotation
+            {Xv2, Yv2} = ow_vector:rotate(Vel1, Rot),
+            % Subtract (negative Y is up) from the original vector
+            Vel3 = {Xv - Xv2, Yv - Yv2},
+            % Check to see if the velocity exceeds max velocity
+            Vel4 = 
+                case ow_vector:length_squared(Vel3) > math:pow(MaxV, 2) of
+                    true ->
+                        ow_vector:scale(ow_vector:normalize(Vel3), MaxV);
                     false ->
                         Vel3
                 end,
@@ -319,30 +358,6 @@ action_table() ->
         end}
     ].
 
-%apply_phys_intent(ID, Phys, GameState) ->
-%    % This function will accept any new input received from the player session
-%    % and update the server appropriately.  We will start by applying new
-%    % Velocity vectors received from the client, but what we really ought to do
-%    % is accept only input commands and use those to calculate new
-%    % velocity/rotation for the entity. TODO.
-%    {_Pos, Vel, Rot} = phys_to_tuple(Phys),
-%    EntityInfo = entity_by_id(ID, GameState),
-%    % Zero out any tiny velocities to hopefully help with floating point
-%    % rounding issues.
-%    NewVel =
-%        case overworld_vector:length_squared(Vel) < 0.01 of
-%           false ->
-%               Vel;
-%           true ->
-%               {0, 0}
-%        end,
-%    % Check to see if the player is moving an entity that doesn't exist
-%    % Otherwise continue to update the phys
-%    Entity2 = EntityInfo#entity{
-%        vel = NewVel,
-%        rot = Rot
-%    },
-%    update_entity(Entity2, GameState).
 
 -spec update_positions(integer(), gamestate()) -> gamestate().
 update_positions(TickRate, GS) ->
@@ -357,15 +372,14 @@ update_positions(TickRate, GS) ->
         #{pos := PosMap, vel := VelMap} = Phys,
         #{ x := Xv, y := Yv } = VelMap,
         #{ x := Xp, y := Yp } = PosMap,
-        TickMs = TickRate / 1000,
-        %Latency = get_latency(E),
-        % TODO: Re-add latency - just call it from the session
-        Latency = 0,
+        TickMs = TickRate/1000,
+        %TickMs = 25,
         NewPos = {
-            Xp + (Xv * (TickMs + Latency)),
-            Yp + (Yv * (TickMs + Latency))
+            Xp + (Xv * TickMs),
+            Yp + (Yv * TickMs) 
         },
-        E#entity{phys = Phys#{pos => ow_vector:vector_map(NewPos)}}
+        NewPhys = Phys#{pos => ow_vector:vector_map(NewPos)},
+        E#entity{phys = NewPhys}
     end,
     NewEnts = [PositionFun(X) || X <- Entities],
     %case NewEnts of
@@ -375,6 +389,7 @@ update_positions(TickRate, GS) ->
     %end,
     GS#gamestate{entities = NewEnts}.
 
+
 new_entity(Handle, ID, GameState) ->
     Entities0 = GameState#gamestate.entities,
     Entity = #entity{
@@ -382,7 +397,7 @@ new_entity(Handle, ID, GameState) ->
         name = Handle,
         phys =
             #{
-                pos => ow_vector:vector_map({0, 0}),
+                pos => ow_vector:vector_map({rand:uniform(1024), rand:uniform(1024)}),
                 vel => ow_vector:vector_map({0, 0}),
                 rot => 0
             },
@@ -399,11 +414,13 @@ new_entity(Handle, ID, GameState) ->
                 % rotation / rotation_factor
                 rotation_fac => 20,
                 speed_fac => 5,
-                max_vel => 100
-            }
+                max_vel => 400
+            },
+        latency = 0
     },
     GameState1 = GameState#gamestate{entities = [Entity | Entities0]},
     {Entity, GameState1}.
+
 
 entity_by_id(ID, GameState) ->
     EList = GameState#gamestate.entities,
@@ -417,16 +434,19 @@ update_entity(Entity, GameState) ->
     EList2 = lists:keystore(ID, #entity.id, EList, Entity),
     GameState#gamestate{entities=EList2}.
 
+
 -spec rm_entity(pos_integer(), gamestate()) -> gamestate().
 rm_entity(ID, GameState) ->
     EList = GameState#gamestate.entities,
     EList2 = lists:keydelete(ID, #entity.id, EList),
     GameState#gamestate{entities=EList2}.
 
+
 phys_to_tuple(#{pos := #{x := Xp, y := Yp}, vel := #{x := Xv, y := Yv}, rot := Rot}) ->
     Pos = {Xp, Yp},
     Vel = {Xv, Yv},
     {Pos, Vel, Rot}.
+
 
 -spec entity_map(entity()) -> map().
 entity_map(Entity) ->
@@ -438,5 +458,24 @@ entity_map(Entity) ->
         stats => Entity#entity.stats
     }.
 
-vector_scale({X,Y}, Scalar) ->
-    {X*Scalar, Y*Scalar}.
+
+-spec get_entities(gamestate()) -> [map()].
+get_entities(GameState) ->
+    Entities = GameState#gamestate.entities,
+    F = fun(E, Acc0) -> 
+                EM = entity_map(E),
+                [ EM | Acc0 ]
+        end,
+    lists:foldl(F, [], Entities).
+
+
+-spec get_entity_phys(gamestate()) -> [map()].
+get_entity_phys(GameState) ->
+    Entities = GameState#gamestate.entities,
+    F = fun(E, Acc0) -> 
+                EM = entity_map(E),
+                %#{ id := ID, phys := Phys } = EM,
+                [ maps:with([id, phys], EM) | Acc0 ]
+        end,
+    EP = lists:foldl(F, [], Entities),
+    EP.
