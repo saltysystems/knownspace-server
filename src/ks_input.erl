1d61a3d569e4f08c0338f9b02a19a156c68e26dd
cmdrk <cmdrk@protonmail.com>
2022-11-27 11:07:38 -0600

    input module now just provides some commons funs for applying and pushing input, but the actual keys and functions related to those keys are in different module files

--- src/ks_input.erl
+++ src/ks_input.erl
@@ -1,22 +1,31 @@
 -module(ks_input).
 
--export([proc_reset/1, insert/3, key_table/0]).
+-export([apply/4, push/3, proc_reset/1]).
 
-% TODO: gen_server this? for on-the-fly updates to the input table O_o
-key_table() ->
-    % This table is the master list of all input functions along with the
-    % associated function that should be applied when pressed
-    [
-        {'IMPULSE_FWD', fun(ID, _, Q) -> ks_phys:apply_move(impulse, fwd, ID, Q) end},
-        {'IMPULSE_REV', fun(ID, _, Q) -> ks_phys:apply_move(impulse, rev, ID, Q) end},
-        {'IMPULSE_LEFT', fun(ID, _, Q) -> ks_phys:apply_move(impulse, left, ID, Q) end},
-        {'IMPULSE_RIGHT', fun(ID, _, Q) -> ks_phys:apply_move(impulse, right, ID, Q) end},
-        {'ROTATE_LEFT', fun(ID, _, Q) -> ks_phys:apply_move(rotate, left, ID, Q) end},
-        {'ROTATE_RIGHT', fun(ID, _, Q) -> ks_phys:apply_move(rotate, right, ID, Q) end},
-        {'ACTION_0', fun(ID, C, Q) -> ks_projectile:create_projectile(ID, C, Q) end}
-    ].
+% Recurse over all of the inputs buffered this tick.
+% One optimization may be to do all of the manipulations here and pass the
+% entity data forward until the final entity update is complete, _then_
+% persist to ETS. Benchmark.
+-spec apply(list(), integer(), list(), ow_ecs:query()) -> ok.
+apply([], _ID, _KeyList, _Query) ->
+    ok;
+apply([Input | Rest], ID, Actions, Query) ->
+    Cursor = maps:get(cursor, Input, undefined),
+    KeyList = maps:get(keys, Input, undefined),
+    Apply =
+        fun(KeyPress) ->
+            case lists:keyfind(KeyPress, 1, Actions) of
+                {KeyPress, Fun} ->
+                    Fun(ID, Cursor, Query);
+                false ->
+                    ok % maybe an input not for us
+            end
+        end,
+    lists:foreach(Apply, KeyList),
+    %% Apply the remaining inputs for this actor
+    apply(Rest, ID, Actions, Query).
 
-insert(Input1, ID, World) ->
+push(Input1, ID, World) ->
     Query = ow_ecs:query(World),
     logger:debug("Adding input component to ~p with data ~p", [ID, Input1]),
     % If there's already input, we need to merge it.
