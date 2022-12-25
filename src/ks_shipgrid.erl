c0184887f3152df5960a4d4c8064c85c668f8744
cmdrk <cmdrk@protonmail.com>
2022-12-25 01:08:35 -0600

    formatter pass

--- src/ks_shipgrid.erl
+++ src/ks_shipgrid.erl
@@ -1,9 +1,9 @@
 -module(ks_shipgrid).
 
-% @doc 
+% @doc
 % The ShipGrid module provides an interface for calculating various Group
 % components as used by the actor module
-% @end 
+% @end
 
 -export([new/2, add/4, match_subcomponents/3]).
 
@@ -14,7 +14,7 @@
 %% Public API
 %%-------------------------------------------------------------------
 
-new(ID,World) ->
+new(ID, World) ->
     G = ow_grid2d:new(?MAX_GRID, ?MAX_GRID),
     Q = ow_ecs:query(World),
     ow_ecs:add_component(shipgrid, G, ID, Q).
@@ -24,30 +24,32 @@ add(Coordinate, ModuleType, ParentID, World) ->
     case ow_ecs:try_component(shipgrid, ParentID, Query) of
         false ->
             {error, no_grid};
-        {Grid, Rest} -> 
+        {Grid, Rest} ->
             Children = ow_ecs:get(children, Rest, []),
             ChildRef = instance_module(ModuleType, 0, ParentID, Query),
-            Children2 = [ ChildRef | Children ],
+            Children2 = [ChildRef | Children],
             Grid2 = ow_grid2d:put(Coordinate, ChildRef, Grid),
             % Update the grid
             Components = [
-                          {shipgrid, Grid2},
-                          {children, Children2}
-                         ],
+                {shipgrid, Grid2},
+                {children, Children2}
+            ],
             ow_ecs:add_components(Components, ParentID, Query)
     end.
 
 match_subcomponents(Component, ParentID, World) ->
     Query = ow_ecs:query(World),
     case ow_ecs:try_component(children, ParentID, Query) of
-        false -> []; % no children, so the match is always emptylist
-        {Children, _Rest} -> 
-            F = 
+        % no children, so the match is always emptylist
+        false ->
+            [];
+        {Children, _Rest} ->
+            F =
                 fun(ChildID) ->
                     case ow_ecs:try_component(Component, ChildID, Query) of
-                        false -> 
+                        false ->
                             false;
-                        {Data, _DataRest} -> 
+                        {Data, _DataRest} ->
                             {true, Data}
                     end
                 end,
@@ -55,10 +57,10 @@ match_subcomponents(Component, ParentID, World) ->
     end.
 
 % WIP..
-%pivot(ID, World) -> 
+%pivot(ID, World) ->
 %    % Calculate the pivot point by first calculating the pivot point of each
 %    % component, then summing the sums.
-%    % 
+%    %
 %    % Assume each cell to be 10px by 10 px, such that a rectangle spanning the
 %    % area is defined as [{-5,-5},{5,-5},{5,5},{-5,5}]
 %    Hitboxes = match_subcomponents(hitbox, ID, World),
@@ -68,7 +70,6 @@ match_subcomponents(Component, ParentID, World) ->
 %            {lists:sum(XList), lists:sum(YList)}
 %        end,
 %    lists:foldl(
-    
 
 %%-------------------------------------------------------------------
 %% Internal functions
@@ -79,9 +80,9 @@ instance_module(Type, Orientation, ParentID, Query) ->
     DefaultModules = ?DEFAULT_MODULES,
     % Components added at instance time
     Components = [
-                  {orientation, Orientation},
-                  {parent, ParentID}
-                 ],
+        {orientation, Orientation},
+        {parent, ParentID}
+    ],
     % Get the relevant module from the include
     DefaultComponents = maps:get(Type, DefaultModules),
     % Create the component instance in ECS
