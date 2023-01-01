916aceac7fee6eff42ed13c740d1fc927c24332c
cmdrk <cmdrk@protonmail.com>
2023-01-01 01:18:54 -0600

    minor interface changes and formatting

--- src/ks_reactor.erl
+++ src/ks_reactor.erl
@@ -1,15 +1,24 @@
 -module(ks_reactor).
 % Ship's reactor. Energy is drained every time a fire is shot, then recharges.
 
--export([proc_reactor/2]).
+-export([new/0, proc_reactor/2]).
+
+new() ->
+    #{
+        cur_reactor => 0,
+        max_reactor => 0,
+        rate_reactor => 0
+    }.
 
 proc_reactor(Query, _ZoneData) ->
     % Match any entities who have input this tick
     Fun =
         fun(ID, Reactor) ->
-            #{cur_reactor := Cur, 
-              max_reactor := Max,
-              rate_reactor := R} = Reactor,
+            #{
+                cur_reactor := Cur,
+                max_reactor := Max,
+                rate_reactor := R
+            } = Reactor,
             Cur1 =
                 if
                     Cur < Max ->
@@ -22,7 +31,7 @@ proc_reactor(Query, _ZoneData) ->
             if
                 Cur =/= Max ->
                     % Changed, so update the table
-                    ow_ecs:add_component(reactor, Reactor#{ cur_reactor => Cur1 }, ID, Query);
+                    ow_ecs:add_component(reactor, Reactor#{cur_reactor => Cur1}, ID, Query);
                 true ->
                     ok
             end
