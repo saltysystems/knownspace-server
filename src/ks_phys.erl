3ccada098555d00d3e1978f1d2afe04fcd05115d
cmdrk <cmdrk@protonmail.com>
2023-03-28 19:16:12 -0500

    model accleration, add min/max rotation

--- src/ks_phys.erl
+++ src/ks_phys.erl
@@ -71,20 +71,18 @@ apply(Type, Direction, ID, World) ->
             impulse ->
                 apply_impulse(Direction, Kinematics, Mass, Thrust);
             rotate ->
-                % TODO: Add MaxRot
                 apply_rotation(Direction, Kinematics, AngularMass, Torque)
         end,
     ow_ecs2:add_component(kinematics, UpdatedKinematics, ID, World).
 
 -spec apply_impulse(direction(), kinematics(), integer(), integer()) -> kinematics().
 apply_impulse(Direction, Kinematics, Mass, Thrust) ->
-    % Apply an impulse in a direction increases or decreases the velocity
-    % vector by a fixed amount.
+    % Apply an impulse in a direction applies an acceleration vector in the
+    % selected direction
     <<LThrust:8, BThrust:8, RThrust:8, TThrust:8>> = Thrust,
-    % Get the current velocity
-    #{vel_t := {Xv, Yv}, pos_r := Rot} = Kinematics,
-    logger:notice("Velocity is: ~p", [{Xv, Yv}]),
-    Vel1 =
+    % Get the current velocity, acceleration and rotation
+    #{acc_t := {Xa, Ya}, pos_r := Rot} = Kinematics,
+    Acc =
         case Direction of
             % The direction of movement is opposite the thrust direction
             % Fwd = Bottom Thruster
@@ -96,15 +94,19 @@ apply_impulse(Direction, Kinematics, Mass, Thrust) ->
             rev ->
                 {0 * TThrust/Mass, -1 * TThrust/Mass};
             left ->
-                {-1 * RThrust/Mass, 0 * RThrust/Mass};
+                {1 * RThrust/Mass, 0 * RThrust/Mass};
             right ->
-                {1 * LThrust/Mass, 0 * LThrust/Mass}
+                {-1 * LThrust/Mass, 0 * LThrust/Mass}
         end,
     % Rotate the keyed vector by the current rotation
-    {Xv2, Yv2} = ow_vector:rotate(Vel1, Rot),
+    %logger:notice("Non-rot Acceleration: ~p", [Acc]),
+    %logger:notice("Rotaation: ~p", [Rot]),
+    {Xa2, Ya2} = ow_vector:rotate(Acc, Rot),
     % Subtract (negative Y is up) from the original vector
-    Vel3 = {Xv - Xv2, Yv - Yv2},
-    Kinematics#{vel_t := Vel3}.
+    Acc1 = {Xa - Xa2, Ya - Ya2},
+    logger:notice("Rotated acceleration vector: ~p~n", [Acc1]),
+    %logger:notice("Rotated acceleration: ~p~n", [{Xa2, Ya2}]),
+    Kinematics#{acc_t := Acc1}.
 
 -spec apply_rotation(left | right, kinematics(), float(), integer()) -> kinematics().
 apply_rotation(Direction, Kinematics, AngularMass, Torque) ->
@@ -114,22 +116,43 @@ apply_rotation(Direction, Kinematics, AngularMass, Torque) ->
             right -> 1.0;
             left -> -1.0
         end,
+    %logger:notice("Torque: ~p", [Torque]),
+    %logger:notice("Angular mass: ~p", [AngularMass]),
     VelR1 = VelR + D * (Torque / AngularMass),
-    Kinematics#{vel_r := VelR1}.
+    %% Set nearly zero to zero.
+    VelR2 = 
+        if 
+            VelR1 < 1.0e-10 andalso VelR1 > -1.0e-10 ->
+                0;
+            true ->
+                VelR1
+        end,
+    Kinematics#{vel_r := VelR2}.
 
 -spec update_rotation([ow_ecs2:entity()], term(), ow_ecs2:world()) -> ok.
 update_rotation([], _ZoneData, _World) ->
     ok;
-update_rotation([{ID, Components} | R], ZD = #{tick_ms := TickMs}, World) ->
+update_rotation([{ID, Components} | R], ZoneData, World) ->
+    #{tick_ms := TickMs, env := Env} = ZoneData,
+    #{max_vel_r := MaxRot } = Env,
     % Now apply the physics
     Kinematics = ow_ecs2:get(kinematics, Components),
-    #{pos_r := RotP, vel_r := RotV, acc_r := RotA} = Kinematics,
+    #{pos_r := RotP, vel_r := RotV} = Kinematics,
     DeltaT = TickMs / 1000,
-    RotV1 = RotV + RotA * DeltaT,
-    RotP1 = RotP + RotV1 * DeltaT,
-    KinematicsUpdate = Kinematics#{pos_r := RotP1, vel_r := RotV1},
+    RotV2 = 
+        case RotV of
+            _ when RotV < -MaxRot ->
+                -MaxRot;
+            _ when RotV > MaxRot ->
+                MaxRot;
+            _ -> 
+                RotV
+        end,
+    RotP1 = RotP + RotV2 * DeltaT,
+    %logger:notice("rotation is: ~p~n", [RotP1]),
+    KinematicsUpdate = Kinematics#{pos_r := RotP1, vel_r := RotV2},
     ow_ecs2:add_component(kinematics, KinematicsUpdate, ID, World),
-    update_rotation(R, ZD, World).
+    update_rotation(R, ZoneData, World).
 
 -spec update_position([ow_ecs2:entity()], term(), ow_ecs2:world()) -> ok.
 update_position([], _ZoneData, _World) ->
@@ -139,17 +162,27 @@ update_position([{ID, Components} | R], ZoneData, World) ->
     #{max_vel_t := MaxVel} = Env,
     % Now apply the physics
     Kinematics = ow_ecs2:get(kinematics, Components),
-    #{pos_t := Pos, vel_t := Vel} = Kinematics,
+    #{pos_t := Pos0, vel_t := Vel0, acc_t := Acc0} = Kinematics,
     DeltaT = TickMs / 1000,
+    % Calculate the new velocity
+    % v = v0 + a*t
+    Vel = ow_vector:add(Vel0,ow_vector:scale(Acc0, DeltaT)),
     % Fix the velocity to the environment maximum
     Vel1 =
-        case ow_vector:length_squared(Vel) > math:pow(MaxVel, 2) of
+        case ow_vector:length_squared(Vel) >= math:pow(MaxVel, 2) of
             true ->
                 ow_vector:scale(ow_vector:normalize(Vel), MaxVel);
             false ->
                 Vel
         end,
-    Pos1 = ow_vector:add(Pos, ow_vector:scale(Vel1, DeltaT)),
-    KinematicsUpdate = Kinematics#{pos_t := Pos1, vel_t := Vel1},
-    ow_ecs2:add_component(kinematics, KinematicsUpdate, ID, World),
+    Pos1 = ow_vector:add(Pos0, ow_vector:scale(Vel1, DeltaT)),
+    % Update the velocity, position
+    % Then set acceleration back to 0 in preparation for the next frame
+    Kinematics1 = Kinematics#{
+                    pos_t := Pos1, 
+                    vel_t := Vel1, 
+                    acc_t := {0,0}
+                   },
+    ow_ecs2:add_component(kinematics, Kinematics1, ID, World),
     update_position(R, ZoneData, World).
+
