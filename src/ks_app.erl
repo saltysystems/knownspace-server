bbd910326a7e610464d878169405962096974ccc
cmdrk <cmdrk@proton.me>
2023-06-18 13:57:13 -0500

    update

--- src/ks_app.erl
+++ src/ks_app.erl
@@ -10,7 +10,13 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    ow_protocol:register_app(ks),
+    ow_protocol:register(
+        #{ 
+            app => ks,
+            router => ks_msg,
+            modules => [ ks_zone2 ]
+         }
+    ),
     ks_sup:start_link().
 
 stop(_State) ->
